748d566fc7bc18a7affe23bd0efa68eee828f598
Pedro <pedromassj@gmail.com>
2018-01-24 20:16:23 -0300

    Me dejaron trabajar en el laburo

---
+++ app/src/main/java/com/example/root/trabajofinal/TiposEnumerados/SubirVideo.java
@@ -0,0 +1,13 @@
+package com.example.root.trabajofinal.TiposEnumerados;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubirVideo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subir_video);
+    }
+}
